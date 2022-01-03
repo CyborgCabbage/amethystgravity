cf027aabe74701029daa24abe147dc92904f12ba
CyborgCabbage <finlaylaidlaw9@gmail.com>
2022-01-03 18:17:05 +0000

    - Add pylon block (with blockstates)

--- src/main/java/cyborgcabbage/amethystgravity/AmethystGravity.java
+++ src/main/java/cyborgcabbage/amethystgravity/AmethystGravity.java
@@ -1,14 +1,27 @@
 package cyborgcabbage.amethystgravity;
 
+import cyborgcabbage.amethystgravity.block.PylonBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class AmethystGravity implements ModInitializer {
-	public static final Logger LOGGER = LogManager.getLogger("amethystgravity");
+	public static final String MODID = "amethystgravity";
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+	public static final Block PYLON_BLOCK = new PylonBlock(FabricBlockSettings.of(Material.WOOD).noCollision().breakInstantly().luminance(state -> 10));
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.BLOCK, new Identifier(MODID, "pylon"), PYLON_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(MODID, "pylon"), new BlockItem(PYLON_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
